95b0c37cf9af0f86e801bf500a7a814bb225fe11
AlexandrescuPaulGeorge <alex.paul15@yahoo.com>
2024-02-22 22:43:46 +0200

    Add project files.

---
+++ Api/Dtos/MessageDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Dtos
+{
+    public class MessageDto
+    {
+
+        [Required]
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+}
